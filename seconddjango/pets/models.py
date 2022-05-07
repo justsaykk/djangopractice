41c1b666e05d3564d52dc809379b941380991602
justsaykk <kaikein.woo@gmail.com>
2022-05-07 12:21:04 +0800

    created Dog model

--- seconddjango/pets/models.py
+++ seconddjango/pets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#The Model class we will inherit from
+from django.db import models
+
+#new model class
+class Dog(models.Model):
+    # define a string field of max 100 characters
+    name = models.CharField(max_length=100)
+    # define a age that is an integer
+    age = models.IntegerField()
