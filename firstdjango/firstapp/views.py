36c1b8f3f4f56c01f527e818bd627c5d2b2a976d
justsaykk <kaikein.woo@gmail.com>
2022-05-07 11:49:24 +0800

    finished intro to opinionated framework

--- firstdjango/firstapp/views.py
+++ firstdjango/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse  # For Sending Json Responses
 from django.views import View  # Allows our class to act as a view
+from .helpers import GetBody
 
 
 class FirstView(View):
@@ -19,3 +20,27 @@ class FirstView(View):
     # since the methods name is "delete" it will run on "delete" requests
     def delete(self, request):
         return JsonResponse({"hello": "world", "method": request.method})
+
+
+class SecondView(View):
+    def get(self, request, param):
+        # Grab query from url query
+        query = request.GET.get("query", "no query")
+        return JsonResponse({"param": param, "query": query})
+
+    def post(self, request, param):
+        query = request.GET.get("query", "no query")
+        return JsonResponse({"param": param, "query": query})
+
+    def put(self, request, param):
+        query = request.GET.get("query", "no query")
+        return JsonResponse({"param": param, "query": query})
+
+    def delete(self, request, param):
+        query = request.GET.get("query", "no query")
+        return JsonResponse({"param": param, "query": query})
+
+
+class ThirdView(View):
+    def post(self, request):
+        return JsonResponse(GetBody(request))
