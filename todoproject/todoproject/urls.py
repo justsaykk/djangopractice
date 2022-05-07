ac72d5d636a0b50cad43683f40c2c41dc4a776c4
justsaykk <kaikein.woo@gmail.com>
2022-05-07 15:22:02 +0800

    added models

--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from todos.views import TodoViewSet
+
+# create a new router
+router = routers.DefaultRouter()
+# register our viewsets
+router.register(r'todos', TodoViewSet) #register "/todos" routes
+
 
 urlpatterns = [
+    # add all of our router urls
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+
