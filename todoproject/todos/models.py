ef52dc0c8c75adcc4c1fbce8654420c7813e666d
justsaykk <kaikein.woo@gmail.com>
2022-05-08 12:50:34 +0800

    updated models

--- todoproject/todos/models.py
+++ todoproject/todos/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    # level = models.IntegerField(default=1)
 class Todo(models.Model):
     subject = models.CharField(max_length=100)
     details = models.CharField(max_length=100)
+    # category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
